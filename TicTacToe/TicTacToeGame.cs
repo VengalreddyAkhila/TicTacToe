4a593c1d987324365e09b4c1e3b8db9a1d65d0bd
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-06 21:54:06 +0530

    Refactor:Adding comments

--- TicTacToe/TicTacToeGame.cs
+++ TicTacToe/TicTacToeGame.cs
@@ -6,7 +6,11 @@ namespace TicTacToe
 {
     static class TicTacToeGame
     {
-        //intialize board 
+        /// <summary>
+        /// TicTacToe game plays in a gameboard 
+        /// </summary>
+        /// <returns></returns>
+        //intializing the board  with empty space
         public static char[] createBoard()
         {
             char[] board = new char[10];
@@ -16,7 +20,7 @@ namespace TicTacToe
             }
             return board;
         }
-        //choose letter
+        //user choosing the option 
         public static char chooseUserChar()
         {
             Console.WriteLine("Enter your choice X or O :");
@@ -24,54 +28,58 @@ namespace TicTacToe
             return char.ToUpper(userSelection);
 
         }
-        //displayboard
+        //displaying the gameboard
         public static void ShowBoard(char[] board)
-        {            
-            Console.WriteLine("  1  |  2  |  3 "); 
-            Console.WriteLine("_____|_____|_____ ");          
+        {
+            Console.WriteLine("  1  |  2  |  3 ");
+            Console.WriteLine("_____|_____|_____ ");
 
-            Console.WriteLine("  4  |  5  |  6"); 
-            Console.WriteLine("_____|_____|_____ ");                 
+            Console.WriteLine("  4  |  5  |  6");
+            Console.WriteLine("_____|_____|_____ ");
 
             Console.WriteLine("  7  |  8  |  9");
             Console.WriteLine("     |     |      ");
         }
-        //user make a move
-        
+        //user makeing a move in gameboard
+
         public static char[] UserMove(char[] board)
         {
             Console.WriteLine("select the location on Board :  ");
-            int location = Convert.ToInt32(Console.ReadLine());
-
+            int location = Convert.ToInt32(Console.ReadLine());            
             while (location > 9 || location < 1)
             {
-                Console.WriteLine("invalid location, select the location on Board :  ");                          
-                if (location  == 0)
-                {
-                    Console.WriteLine("location already selected");
-                    Console.WriteLine("select another location on Board :  ");
-                    location = Convert.ToInt32(Console.ReadLine());
-                }
-                else
-                {
-                    board[location] = 'x';
-                }
+                Console.WriteLine("invalid location, select the location on Board :  ");
+                location = Convert.ToInt32(Console.ReadLine());
             }
-            return board;
-        }
-
-        private static int checkBoard(int location, char[] board)
-        {
-            if (board[location] != ' ')
+            location = CheckBoard(location, board);
+            if (location == 0)
             {
-                return 0;
+                Console.WriteLine("location already selected");
+                Console.WriteLine("select another location on Board :  ");
+                location = Convert.ToInt32(Console.ReadLine());
             }
             else
             {
-                return location;
+                board[location] = 'x';
             }
-        }
+            return board;
+        }       
+
+        private static int CheckBoard(int location, char[] board)
+            {
+                if (board[location] != ' ')
+                {
+                    return 0;
+                }
+                else
+                {
+                    return location;
+                }
+            }
+        
     }
-    
-    
 }
+    
+    
+    
+
